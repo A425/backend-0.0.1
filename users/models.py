d5e226337c38913ea4bda61d14a919cdc1dcb670
liuyuchen <liu170045@gmail.com>
2014-12-28 18:37:57 +0800

    update

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
-# from django.db import models
+from django.db import models
 
-# class Token(models.Model):
-#     name = models.CharField(max_length=64)
-#     token = models.CharField(max_length=64)
+class Token(models.Model):
+    name = models.CharField(max_length=64)
+    token = models.CharField(max_length=64)
