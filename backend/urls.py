eebbf45e838e73511cb26dd288af6e7552cc7374
liuyuchen <liu170045@gmail.com>
2015-01-03 19:49:34 +0800

    update

--- backend/urls.py
+++ backend/urls.py
@@ -23,7 +23,7 @@ urlpatterns = patterns('',
 
     url(r'^postArticle$', postArticle),
     url(r'^getArticles$', getArticles),
-    url(r'^testArticle/$', testArticle)
+    url(r'^testArticle/$', testArticle),
 
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT }),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.STATIC_ROOT })
 )
